024783c1eea1ddd7e5d6843e66693fc2574223a1
Maximilien <pierremaximilien38@gmail.com>
2020-07-15 13:33:34 +0200

    initial js

---
+++ javavascript.js
@@ -0,0 +1,40 @@
+(() => {
+
+    // variable 
+
+    let cookie = 1,multi = 2,price=10,lifecookie=0, currentcookie = 0;
+
+    // Multiplicateur
+
+    document.getElementById("multi").addEventListener("click", () => {
+
+				if (price<=currentcookie) {
+        cookie = cookie*multi;
+        currentcookie = currentcookie-price;
+        multi = multi*2;
+        price = price*price;
+        }
+        
+        document.getElementById("showcookie").innerHTML = cookie;
+        document.getElementById("showprice").innerHTML = "cost " + price + " cookies";
+        document.getElementById("target").innerHTML = "Current " + currentcookie + " Cookies";
+
+
+    });
+    
+    // cookie click
+
+
+    document.getElementById("cookie").addEventListener("click", () => {
+
+        currentcookie += cookie;
+        currentcookie = Math.round(currentcookie);
+        lifecookie += cookie;
+        lifecookie = Math.round(lifecookie);
+        document.getElementById("target").innerHTML = "Current " + currentcookie + " Cookies";
+        document.getElementById("life").innerHTML = lifecookie + " lifetime Cookies";
+
+
+    });
+
+})()
